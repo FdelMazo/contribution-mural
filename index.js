504e0da86cd3ab084934588f742a9eb70bc1b277
FdelMazo <federicodelmazo@hotmail.com>
2018-09-30 18:56:18 -0300

    Agrego contadores de PRs

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var link_default = "<a href='https://github.com/FdelMazo/contribution-mural'>"
 var texto_default = "<img class='img-icon' src='favicon.ico'> Visiten el repo para contribuir! <img class='img-icon' src='favicon.ico'>"
+var API = "https://api.github.com/repos/fdelmazo/contribution-mural/pulls?state=all"
 
 $(document).ready(function(){
 
-    // Init
+    hit_API()
+
     var clicked = false
     var commit = document.getElementById("commit");
     commit.innerHTML = link_default + texto_default;
@@ -13,20 +15,21 @@ $(document).ready(function(){
             'data-author': img.data_author,
             alt: img.alt,
             mouseover: function(ev) {
-                update_msg(ev,true)
+                if(clicked) return
+                update_msg(ev)
             },
             click: function(ev) {
-                update_msg(ev,false);
-                cliquear();
+                update_msg(ev);
+                clicked = !clicked
             },
             mouseleave: function() {
+                if(clicked) return;
                 clean_msg();
             }
         }).appendTo('body');
     });
 
-    function update_msg(evt,bymouseover){
-        if(clicked && bymouseover) return; // Si clickee y entre a traves de mouseover, no quiero perder el msg clickeado.
+    function update_msg(evt){
         var imagen = evt.target
         var msg = imagen.alt;
         var author = $(imagen).data('author')
@@ -34,13 +37,33 @@ $(document).ready(function(){
         commit.innerHTML = author_link + ' dice: ' + msg;
     }
 
-    function cliquear(){
-        clicked = !clicked
-    }
-
     function clean_msg(){
-        if(clicked) return;
         commit.innerHTML = link_default + texto_default;
     }
+
+    function hit_API(){
+        $.ajax({
+            url: API,
+            method: 'GET',
+            success: function(data) {
+                update_PRs(data)
+            }
+        })
+    }
+
+    function update_PRs(data){
+        var open = $('#pr-open')
+        var closed = $('#pr-closed')
+        var open_count = 0
+        var closed_count = 0
+        data.forEach(pr => {
+            if (pr.state == "closed") closed_count++
+            else if (pr.state == "open") open_count++
+        })
+        open.text(open_count)
+        closed.text(closed_count)
+    }
+    
+
 })
 
