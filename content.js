70c42397213095c15b4567819d159de90b8b9021
Esteban Djeordjian <edjeordjian@gmail.com>
2018-10-01 23:01:16 -0300

    Aporte discrecional

--- content.js
+++ content.js
@@ -114,4 +114,17 @@ var content = [
     "data_author": "xNicky",
     "alt": "No sabés usar Git?"
   },
+  
+  {
+    "src": "media/cosoColorinche.gif",
+    "data_author": "edjeordjian",
+    "alt": "pa k kieres saber eso jaja saludos."
+  },
+  
+  
+  {
+    "src": "media/elmo.gif",
+    "data_author": "edjeordjian",
+    "alt": "ira men deja te esplico yo no vengo ofendiendo a nadien carnal y estoi tratando de exponer un punto de bista y llegas con inzultos y pues asi no se puede men si te pido respeto xqe tambien ay damas que estan viendo tus insultos sale? grasias d anthemano﻿"
+  },
 ];
